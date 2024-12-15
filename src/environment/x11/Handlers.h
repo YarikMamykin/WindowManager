26ebdc3c27068a8ab5315b4157a91a21b323b94d
YarikMamykin <ymamykin@gmail.com>
2024-12-15 19:31:11 +0200

    Add colormap to x11 handlers

--- src/environment/x11/Handlers.h
+++ src/environment/x11/Handlers.h
@@ -5,14 +5,19 @@
 #include <X11/Xutil.h>
 #include <X11/cursorfont.h>
 #include <X11/keysym.h>
+#include <unordered_map>
 // clang-format on
 
 namespace ymwm::environment {
+  enum class ColorID { WindowBorder = 1 };
+
   struct Handlers {
     Display* display;
     int screen;
     Window root_window;
     Cursor cursor;
+    Colormap colormap;
+    std::unordered_map<ColorID, XColor> colors;
 
     inline Handlers() {
       display = XOpenDisplay(nullptr);
@@ -22,9 +27,13 @@ namespace ymwm::environment {
       screen = DefaultScreen(display);
       root_window = RootWindow(display, screen);
       cursor = XCreateFontCursor(display, XC_left_ptr);
+      colormap = XCreateColormap(
+          display, root_window, DefaultVisual(display, screen), AllocNone);
+      colors.reserve(1);
     }
 
     inline ~Handlers() {
+      XFreeColormap(display, colormap);
       XFreeCursor(display, cursor);
       XDestroyWindow(display, root_window);
       XSync(display, 0);
