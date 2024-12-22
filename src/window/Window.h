754e9a572d30eee4e32e6efbd7fe571fbf262beb
YarikMamykin <ymamykin@gmail.com>
2024-12-22 18:11:10 +0200

    Add default values and comparison operator to Window

--- src/window/Window.h
+++ src/window/Window.h
@@ -5,12 +5,15 @@
 
 namespace ymwm::window {
   struct Window {
-    environment::ID id;
-    int x;
-    int y;
-    int w;
-    int h;
-    int border_width;
-    environment::ColorID border_color;
+    environment::ID id{ 0 };
+    int x{ 0 };
+    int y{ 0 };
+    int w{ 0 };
+    int h{ 0 };
+    int border_width{ 0 };
+    environment::ColorID border_color{ environment::ColorID::Red };
+
+    inline constexpr bool
+    operator<=>(const Window& other) const noexcept = default;
   };
 } // namespace ymwm::window
