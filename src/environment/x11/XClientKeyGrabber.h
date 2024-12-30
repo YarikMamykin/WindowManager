41a73bb2a1e7657dd83980df60b3524d43e51bb8
YarikMamykin <ymamykin@gmail.com>
2024-12-30 03:21:39 +0200

    Separate Client Key grabbing

---
+++ src/environment/x11/XClientKeyGrabber.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "environment/x11/Handlers.h"
+#include "events/AbstractKeyPress.h"
+
+#include <iostream>
+
+namespace ymwm::environment {
+  struct XClientKeyGrabber {
+    Handlers* handlers;
+
+    template <typename EventType>
+    inline void operator()(const EventType& event) {
+      std::cerr << "Unexpected event type: " << event.type << "\n";
+    }
+
+    template <>
+    inline void operator()(const events::AbstractKeyPress& event) {
+      std::cout << "Grabbing keys: " << std::hex << event.code << " "
+                << event.mask << "\n";
+      XGrabKey(handlers->display,
+               XKeysymToKeycode(handlers->display, event.code),
+               event.mask,
+               handlers->root_window,
+               true,
+               GrabModeAsync,
+               GrabModeAsync);
+    }
+  };
+} // namespace ymwm::environment
