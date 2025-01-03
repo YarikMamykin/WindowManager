dcf0727ced818068d9ec5e6e23a7416165c8cb90
Yaroslav Mamykin (GitHub) <33784535+YarikMamykin@users.noreply.github.com>
2025-01-03 16:13:27 +0200

    Add move, focus, change border color, etc. commands

--- src/environment/x11/Commands.cpp
+++ src/environment/x11/Commands.cpp
@@ -1,7 +1,45 @@
 #include "environment/Command.h"
 #include "environment/Environment.h"
 
+#include <cstdlib>
+#include <thread>
+
 namespace ymwm::environment::commands {
+
   void Dummy::execute([[maybe_unused]] Environment&) const {}
   void ExitRequested::execute(Environment& e) const { e.request_exit(); }
+  void RunTerminal::execute(Environment& e) const {
+    std::thread([]() -> void {
+      std::system("/usr/bin/alacritty -o \"window.dimensions={columns=20, "
+                  "lines=20}\"");
+    }).detach();
+  }
+
+  void ChangeBorderColor::execute(Environment& e) const {
+    e.manager().change_border_color(color);
+  }
+
+  void MoveWindowRight::execute(Environment& e) const {
+    e.manager().move_focused_window_to(100, 0);
+  }
+
+  void CloseWindow::execute(Environment& e) const {
+    e.manager().close_focused_window();
+  }
+
+  void FocusNextWindow::execute(Environment& e) const {
+    e.manager().focus_next_window();
+  }
+
+  void FocusPrevWindow::execute(Environment& e) const {
+    e.manager().focus_prev_window();
+  }
+
+  void MoveFocusedWindowForward::execute(Environment& e) const {
+    e.manager().move_focused_window_forward();
+  }
+
+  void MoveFocusedWindowBackward::execute(Environment& e) const {
+    e.manager().move_focused_window_backward();
+  }
 } // namespace ymwm::environment::commands
