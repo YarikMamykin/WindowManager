bee4810027775b93301d9b4cfbc53f3f17003d82
YarikMamykin <ymamykin@gmail.com>
2025-01-09 17:04:23 +0200

    Remove `static` modifier from Layout config

--- src/config/Layout.h
+++ src/config/Layout.h
@@ -9,30 +9,29 @@ namespace ymwm::config::layouts {
   };
 
   namespace maximized {
-    static constinit inline Margin screen_margins{ .left = 0u,
-                                                   .right = 0u,
-                                                   .top = 0u,
-                                                   .bottom = 0u };
+    constinit inline Margin screen_margins{ .left = 0u,
+                                            .right = 0u,
+                                            .top = 0u,
+                                            .bottom = 0u };
   }
 
   namespace grid {
-    static constinit inline Margin screen_margins{ .left = 0u,
-                                                   .right = 0u,
-                                                   .top = 0u,
-                                                   .bottom = 0u };
+    constinit inline Margin screen_margins{ .left = 0u,
+                                            .right = 0u,
+                                            .top = 0u,
+                                            .bottom = 0u };
 
     struct Margins {
       unsigned int horizontal;
       unsigned int vertical;
     };
 
-    static constinit inline Margins grid_margins{ .horizontal = 0u,
-                                                  .vertical = 0u };
+    constinit inline Margins grid_margins{ .horizontal = 0u, .vertical = 0u };
   } // namespace grid
 
   namespace stack_vertical_right {
-    static constinit inline unsigned int main_window_ratio{ 50 };
-    static constinit inline unsigned int main_window_margin{ 5 };
-    static constinit inline unsigned int stack_window_margin{ 5 };
+    constinit inline unsigned int main_window_ratio{ 50 };
+    constinit inline unsigned int main_window_margin{ 5 };
+    constinit inline unsigned int stack_window_margin{ 5 };
   } // namespace stack_vertical_right
 } // namespace ymwm::config::layouts
