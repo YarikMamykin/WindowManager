3c042c4c381b42109b419c6372cce551eddc837b
YarikMamykin <ymamykin@gmail.com>
2025-01-11 20:44:18 +0200

    Remove parsing from `events` lib

--- src/events/Map.cpp
+++ src/events/Map.cpp
@@ -1,14 +1,9 @@
 #include "Map.h"
 
-#include "YamlModels.h"
+#include "AbstractKeyCode.h"
+#include "AbstractKeyMask.h"
 #include "environment/Command.h"
 #include "events/AbstractKeyPress.h"
-#include "utils.h"
-
-#include <iostream>
-#include <variant>
-#include <yaml-cpp/exceptions.h>
-#include <yaml-cpp/yaml.h>
 
 namespace ymwm::events {
   Map default_event_map() noexcept {
@@ -62,52 +57,4 @@ namespace ymwm::events {
     return bindings;
   }
 
-  std::variant<Map, std::string>
-  event_map_from_yaml(std::filesystem::path&& yamlpath) noexcept {
-    Map event_map;
-    try {
-      YAML::Node config = YAML::LoadFile(yamlpath);
-      YAML::Node key_bindings = config["key-bindings"];
-      if (not key_bindings.IsSequence()) {
-        std::cout << "key-bindings must be sequence\n";
-        return event_map;
-      }
-
-      for (const auto& binding : key_bindings) {
-        YAML::Node type = binding["type"];
-        if (not type) {
-          std::cout << "Type of key binding must be specified\n";
-          continue;
-        }
-
-        if (auto type_value = type.as<std::string>();
-            AbstractKeyPress::type == type_value) {
-
-          auto key_press_event = binding.as<AbstractKeyPress>();
-
-          if (not binding["cmd"]) {
-            return "Command is not specified for key binding";
-          }
-
-          auto cmd = utils::command_from_type(
-              binding["cmd"]["name"].as<std::string>());
-          if (not cmd) {
-            return "Unknown command specified";
-          }
-
-          if (auto cmd_args = binding["cmd"]["args"]) {
-            utils::fill_cmd_args(*cmd, cmd_args);
-          }
-
-          event_map.insert(std::make_pair(key_press_event, *cmd));
-        }
-      }
-
-    } catch (const YAML::BadFile& e) {
-      std::cout << e.what() << "\n";
-    } catch (const YAML::Exception& e) {
-      std::cout << e.what() << "\n";
-    }
-    return event_map.empty() ? default_event_map() : event_map;
-  }
 } // namespace ymwm::events
