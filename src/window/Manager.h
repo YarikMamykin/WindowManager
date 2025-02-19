dd9b3c064c7f07128fe91f3258be093a1dc7e9a7
YarikMamykin <ymamykin@gmail.com>
2025-02-19 23:55:18 +0200

    Revert "Add `active` flag to Manager"
    This reverts commit 1f83d45991fb9109798e1bfa04ad113bf6bfef19.

--- src/window/Manager.h
+++ src/window/Manager.h
@@ -28,8 +28,7 @@ namespace ymwm::window {
                                     this,
                                     config::windows::focused_border_width,
                                     config::windows::focused_border_color))
-        , m_layout_manager(m_windows, env)
-        , m_active(true) {
+        , m_layout_manager(m_windows, env) {
       m_windows.reserve(5);
 
       layout().update(layouts::Centered{});
@@ -137,7 +136,6 @@ namespace ymwm::window {
     inline void activate() noexcept {
       layout().update();
       focus().update();
-      m_active = true;
     }
 
     inline void deactivate() noexcept {
@@ -145,11 +143,7 @@ namespace ymwm::window {
         w.x = 0 - w.w * 2;
         m_env->move_and_resize(w);
       }
-
-      m_active = false;
     }
-
-    inline bool active() const noexcept { return m_active; }
 
     inline FocusManager<Environment>& focus() noexcept {
       return m_focus_manager;
@@ -165,6 +159,5 @@ namespace ymwm::window {
     layouts::Layout m_layout;
     FocusManager<Environment> m_focus_manager;
     LayoutManager<Environment> m_layout_manager;
-    bool m_active;
   };
 } // namespace ymwm::window
