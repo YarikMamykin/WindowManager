bb42b6dadd5feff153b2bb83653fe443b8a9dfec
YarikMamykin <ymamykin@gmail.com>
2025-02-22 21:13:34 +0200

    Grab mouse buttons as well as keys on start

--- src/environment/x11/XClientKeyGrabber.h
+++ src/environment/x11/XClientKeyGrabber.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "environment/x11/Handlers.h"
 #include "events/AbstractKeyPress.h"
+#include "events/AbstractMousePress.h"
 
 #include <iostream>
 
@@ -22,6 +23,23 @@ namespace ymwm::environment {
                true,
                GrabModeAsync,
                GrabModeAsync);
+    }
+
+    template <>
+    inline void operator()(const events::AbstractMousePress& event) {
+      std::cout << "Grabbing buttons: " << std::hex << event.mcode << " "
+                << event.mask << "\n";
+
+      XGrabButton(handlers->display,
+                  event.mcode,
+                  event.mask,
+                  handlers->root_window,
+                  false,
+                  ButtonPressMask | ButtonReleaseMask,
+                  GrabModeAsync,
+                  GrabModeSync,
+                  None,
+                  None);
     }
   };
 } // namespace ymwm::environment
