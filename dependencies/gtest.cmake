f12f53b7cc5944b22832952ee812ee1594d5da13
Yaroslav Mamykin (GitHub) <33784535+YarikMamykin@users.noreply.github.com>
2025-06-09 01:44:29 +0300

    Update cmake dependencies module for cmake 4.0 compatibility (#104)

--- dependencies/gtest.cmake
+++ dependencies/gtest.cmake
@@ -1,35 +1,30 @@
+include(ExternalProject)
+
 set(GTEST_DIR ${CMAKE_BINARY_DIR}/googletest)
-set(GTEST_INSTALL_DIR ${CMAKE_BINARY_DIR}/gtest_lib)
+set(GTEST_INSTALL_DIR ${CMAKE_BINARY_DIR}/libs/gtest)
 set(GTEST_REPO https://github.com/google/googletest.git)
+set(GTEST_INCLUDE_DIRS ${GTEST_INSTALL_DIR}/include)
 
-list(APPEND GTEST_INCLUDE_DIRS 
-	${GTEST_DIR}/googlemock/include 
-	${GTEST_DIR}/googletest/include
-)
-
-list(APPEND GTEST_LIBRARIES 
+list(APPEND GTEST_LIBRARIES
 	${GTEST_INSTALL_DIR}/lib/libgtest.a
 	${GTEST_INSTALL_DIR}/lib/libgtest_main.a
 	${GTEST_INSTALL_DIR}/lib/libgmock.a
 	${GTEST_INSTALL_DIR}/lib/libgmock_main.a
 )
 
-if(NOT EXISTS ${GTEST_DIR})
-	add_custom_command(OUTPUT ${GTEST_DIR} ${GTEST_INCLUDE_DIRS}
-		COMMAND git clone ${GTEST_REPO}
-		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-	)
-endif()
-
-add_custom_command(OUTPUT ${GTEST_LIBRARIES} 
-	COMMAND cmake -S . -B build -DBUILD_GMOCK=ON -DCMAKE_INSTALL_PREFIX=${GTEST_INSTALL_DIR}
-	COMMAND cmake --build build -- -j `nproc`
-	COMMAND cmake --install build
-	DEPENDS ${GTEST_DIR}
-	WORKING_DIRECTORY ${GTEST_DIR}
+ExternalProject_Add(googletest_lib
+    GIT_REPOSITORY ${GTEST_REPO}
+    GIT_TAG v1.17.0
+    PREFIX ${GTEST_DIR}
+    BINARY_DIR ${GTEST_DIR}/build
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${GTEST_INSTALL_DIR}
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+				-DBUILD_GMOCK=ON 
+    BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --parallel
+    INSTALL_COMMAND ${CMAKE_COMMAND} --install <BINARY_DIR>
 )
 
-add_custom_target(googletestlib DEPENDS
-	${GTEST_DIR}
-	${GTEST_LIBRARIES}
-)
+add_library(googletestlib INTERFACE IMPORTED)
+add_dependencies(googletestlib googletest_lib)
+target_include_directories(googletestlib INTERFACE ${GTEST_INCLUDE_DIRS})
--- dependencies/yaml-cpp.cmake
+++ dependencies/yaml-cpp.cmake
@@ -1,22 +1,29 @@
+include(ExternalProject)
+
 set(YAML_DIR ${CMAKE_BINARY_DIR}/yaml-cpp)
-set(YAML_INCLUDE_DIR ${YAML_DIR}/include)
-set(YAML_LIB ${CMAKE_INSTALL_PREFIX}/lib/libyaml-cpp.a)
+set(YAML_INSTALL_DIR ${CMAKE_BINARY_DIR}/libs/yaml-cpp)
+set(YAML_INCLUDE_DIR ${YAML_INSTALL_DIR}/include)
+if(CMAKE_BUILD_TYPE MATCHES DEBUG)
+    set(YAML_LIB ${YAML_INSTALL_DIR}/lib/libyaml-cppd.a)
+elseif(CMAKE_BUILD_TYPE MATCHES RELEASE)
+    set(YAML_LIB ${YAML_INSTALL_DIR}/lib/libyaml-cpp.a)
+endif()
 set(YAML_REPO https://github.com/jbeder/yaml-cpp.git)
 
-add_custom_command(OUTPUT ${YAML_DIR}
-	COMMAND git clone ${YAML_REPO}
-	WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+ExternalProject_Add(yamlcpplib
+    GIT_REPOSITORY ${YAML_REPO}
+    GIT_TAG master
+    PREFIX ${YAML_DIR}
+    BINARY_DIR ${YAML_DIR}/build
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${YAML_INSTALL_DIR}
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+				-DYAML_BUILD_SHARED_LIBS=OFF 
+				-DYAML_CPP_BUILD_TESTS=OFF
+    BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --parallel
+    INSTALL_COMMAND ${CMAKE_COMMAND} --install <BINARY_DIR>
 )
 
-add_custom_command(OUTPUT ${YAML_LIB}
-	COMMAND cmake -S . -B ${CMAKE_BUILD_TYPE} -DYAML_BUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DYAML_CPP_BUILD_TESTS=OFF
-	COMMAND cmake --build ${CMAKE_BUILD_TYPE} --parallel
-	COMMAND cmake --install ${CMAKE_BUILD_TYPE}
-	DEPENDS ${YAML_DIR}
-	WORKING_DIRECTORY ${YAML_DIR}
-)
-
-add_custom_target(yaml_cpp 
-	DEPENDS ${YAML_DIR}
-	DEPENDS ${YAML_LIB}
-)
+add_library(yaml_cpp INTERFACE IMPORTED)
+add_dependencies(yaml_cpp yamlcpplib)
+target_include_directories(yaml_cpp INTERFACE ${YAML_INCLUDE_DIR})
